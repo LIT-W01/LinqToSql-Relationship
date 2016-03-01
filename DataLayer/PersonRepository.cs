6f24c19c322fa0cb37e12fee5627660e7075b86b
Avrumi Friedman <info@lakewoodprogramming.com>
2016-02-29 20:15:45 -0500

    Initial Commit

---
+++ DataLayer/PersonRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class PersonRepository
+    {
+        private string _connectionString;
+
+        public PersonRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Person GetPersonById(int id)
+        {
+            using (PersonCarsDataContext dataContext = new PersonCarsDataContext(_connectionString))
+            {
+                DataLoadOptions loadOptions = new DataLoadOptions();
+                loadOptions.LoadWith<Person>(p => p.Cars);
+                dataContext.LoadOptions = loadOptions;
+                return dataContext.Persons.First(p => p.Id == id);
+            }
+
+            //PersonCarsDataContext dataContext = new PersonCarsDataContext(_connectionString);
+            //Person result = dataContext.Persons.First(p => p.Id == id);
+            //dataContext.Dispose();
+            //return result;
+        }
+    }
+}
---
+++ MvcApplication13/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace MvcApplication13.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int pid)
+        {
+            var repo = new PersonRepository(Properties.Settings.Default.ConStr);
+            return View(repo.GetPersonById(pid));
+        }
+
+    }
+}
